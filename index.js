9e57c8d9238f4dac9a693cb11860c486b3e14fb0
laptop-m8du3f9f <quangtran214203@gmail.com>
2025-02-09 10:58:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+const CHANNEL_ID = process.env.CHANNEL_ID // Thay bằng ID kênh muốn gửi thông báo
+
+client.once('ready', () => {
+  console.log(`Bot đã hoạt động với tên ${client.user.tag}`);
+});
+
+// Khi GitHub gửi webhook
+app.post('/github-webhook', (req, res) => {
+  const payload = req.body;
+  if (payload.pusher) {
+    const commitMessage = payload.head_commit.message;
+    const commitAuthor = payload.head_commit.author.name;
+    const commitUrl = payload.head_commit.url;
+    
+    const message = `🚀 **Commit mới!**\n📌 **Người commit**: ${commitAuthor}\n📝 **Message**: ${commitMessage}\n🔗 **Xem chi tiết**: [Click vào đây](${commitUrl})`;
+
+    const channel = client.channels.cache.get(CHANNEL_ID);
+    if (channel) {
+      channel.send(message);
+    }
+  }
+  res.sendStatus(200);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+// Chạy server trên cổng 3000
+app.listen(3000, () => console.log('Server đang chạy trên cổng 3000'));
